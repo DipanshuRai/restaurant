95f0bfc663b63874867db0e23273b50fb33dc14f
Shouraya16 <shouraya.mishra1604@gmail.com>
2024-06-13 13:41:43 +0530

    Updated Signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser, FaLock, FaPhoneSquareAlt } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
@@ -8,17 +8,42 @@ import SubmitButton from './Forms/SubmitButton';
 import "./Forms/form.css";
 
 export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <div className="form-body">
       <div className='main'>
         <form action="">
           <Header title="Sign Up"/>
-          <InputBox type="text" placeholder="Username" icon={FaUser} />
-          <InputBox type="email" placeholder="Email Address" icon={IoMdMail} />
-          <InputBox type="password" placeholder="Password" icon={FaLock} />
-          <InputBox type="tel" placeholder="Phone Number" icon={FaPhoneSquareAlt} />
+          <InputBox 
+            type="text" 
+            placeholder="Username" 
+            icon={FaUser} 
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <InputBox 
+            type="email" 
+            placeholder="Email Address" 
+            icon={IoMdMail} 
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputBox 
+            type="password" 
+            placeholder="Password" 
+            icon={FaLock} 
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputBox 
+            type="tel" 
+            placeholder="Phone Number" 
+            icon={FaPhoneSquareAlt} 
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
           <SubmitButton submissionType='Sign Up'/>
-          <div className="login-link">
+          <div className="signup-link">
             <p>Already have an account? <Link to="/Login">Log In</Link></p>
           </div>
         </form>
