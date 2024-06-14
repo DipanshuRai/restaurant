58fa67cbd116d2531632bcc810b32fe2d70b08bd
DipanshuRai <157129062+DipanshuRai@users.noreply.github.com>
2024-06-15 00:49:00 +0530

    Added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+// Use command : npm install @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+import Title from './Navbar/Title'
+import Navlink from './Navbar/Navlink'
+import SignBtn from './Navbar/SignBtn'
+import './Navbar/navbar.css'
+
+function Navbar() {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const renderNavLinks = () => (
+    <>
+      <Navlink to="/">Home</Navlink>
+      <Navlink to="/Menu">Menu</Navlink>
+      <Navlink to="/About Us">About Us</Navlink>
+      <Navlink to="/Our Chefs">Our Chefs</Navlink>
+      <Navlink to="/Contact">Contact</Navlink>
+    </>
+  );
+
+  return (
+    <header>
+      <div className="navbar">
+        <Title title="Restaurant" />
+        <ul className='links'>
+          {renderNavLinks()}
+        </ul>
+        <SignBtn />
+        <div className="toggle_btn" onClick={toggleDropdown}>
+          <FontAwesomeIcon icon={isDropdownOpen ? faXmark : faBars} />
+        </div>
+      </div>
+        <div className={`dropdown_menu ${isDropdownOpen ? 'open' : ''}`}>
+          {renderNavLinks()}
+          <li><SignBtn /></li>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
