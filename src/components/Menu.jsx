d22bf0fa56a092ba5e8a7716a8f3c3a9aa32d1d1
NISHANT <iampruh887@gmail.com>
2024-06-15 17:19:33 +0530

    updated code

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './css/Menu.css';
 
-// Import images (replace with your actual image paths)
 import coldCoffee from '../assets/menu-images/beverages/cold-coffee.jpg';
 import hotChocolate from '../assets/menu-images/beverages/hot-chocolate.jpg';
 import mangoSmoothie from '../assets/menu-images/beverages/mango-smoothie.webp';
@@ -115,14 +114,12 @@ const Menu = () => {
             <div className="category-content">
               <h3>{category.charAt(0).toUpperCase() + category.slice(1)}</h3>
             </div>
-            {/* Display images for beverages and desserts */}
             {category === 'beverages' && categoryImages.beverages && (
               <div className="beverage-image" style={{ backgroundImage: `url(${categoryImages.beverages[Math.floor(Math.random() * categoryImages.beverages.length)]})` }}></div>
             )}
             {category === 'desserts' && categoryImages.desserts && (
               <div className="dessert-image" style={{ backgroundImage: `url(${categoryImages.desserts[Math.floor(Math.random() * categoryImages.desserts.length)]})` }}></div>
             )}
-            {/* For other categories */}
             {categoryImages[category] && category !== 'beverages' && category !== 'desserts' && (
               <div
                 className={`${category}-image`}
